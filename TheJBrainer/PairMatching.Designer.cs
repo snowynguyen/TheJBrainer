0dd96056e348eb8f75bb8f164945d0c6b1db4cfa
snowynguyen <nguyennhatnam11022003@gmail.com>
2019-02-20 05:49:27 +0700

    PairMatch - dev started

---
+++ TheJBrainer/PairMatching.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TheJBrainer
+{
+    partial class PairMatching
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GameplayPnl = new System.Windows.Forms.Panel();
+            this.PlayBtn = new System.Windows.Forms.Button();
+            this.GameplayPnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GameplayPnl
+            // 
+            this.GameplayPnl.Controls.Add(this.PlayBtn);
+            this.GameplayPnl.Location = new System.Drawing.Point(12, 12);
+            this.GameplayPnl.Name = "GameplayPnl";
+            this.GameplayPnl.Size = new System.Drawing.Size(880, 480);
+            this.GameplayPnl.TabIndex = 0;
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.Location = new System.Drawing.Point(400, 220);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(80, 40);
+            this.PlayBtn.TabIndex = 0;
+            this.PlayBtn.Text = "button1";
+            this.PlayBtn.UseVisualStyleBackColor = true;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // PairMatching
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 499);
+            this.Controls.Add(this.GameplayPnl);
+            this.Name = "PairMatching";
+            this.Text = "PairMatching";
+            this.Load += new System.EventHandler(this.PairMatching_Load);
+            this.GameplayPnl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel GameplayPnl;
+        private System.Windows.Forms.Button PlayBtn;
+    }
+}
---
+++ TheJBrainer/PairMatching.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheJBrainer
+{
+    public partial class PairMatching : Form
+    {
+        public PairMatching()
+        {
+            InitializeComponent();
+        }
+
+        private void PairMatching_Load(object sender, EventArgs e)
+        {
+            PlayBtn.Visible = true;
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public enum PairMatching_Difficulty
+    {
+        EASY = 0,
+        HARD = 1
+    }
+
+    public enum BasicOperator
+    {
+        PLUS = 1,
+        MINUS = 2,
+        MULTIPLY = 3,
+        DIVIDE = 4
+    }
+
+    public static class Utilities
+    {
+        public static string GetBasicOperatorAsString(BasicOperator _operator)
+        {
+            switch (_operator)
+            {
+                case BasicOperator.PLUS:
+                    return "+";
+                case BasicOperator.MINUS:
+                    return "-";
+                case BasicOperator.MULTIPLY:
+                    return "*";
+                case BasicOperator.DIVIDE:
+                    return "/";
+                default:
+                    return "";
+            }
+        }
+    }
+
+    public class PairMathcing_Question
+    {
+        public int Operand1, Operand2;
+        public BasicOperator Operator;
+
+        public PairMathcing_Question(int operand1, int operand2, BasicOperator _operator)
+        {
+            Operand1 = operand1;
+            Operand2 = operand2;
+            Operator = _operator;
+        }
+
+        public string GetQuestionAsString()
+        {
+            return Operand1.ToString() + Utilities.GetBasicOperatorAsString(Operator) + Operand2.ToString();
+        }
+
+        public string GetAnswerAsString()
+        {
+            int Answer = 0;
+            switch (Operator)
+            {
+                case BasicOperator.PLUS:
+                    Answer = Operand1 + Operand2;
+                    break;
+                case BasicOperator.MINUS:
+                    Answer = Operand1 - Operand2;
+                    break;
+                case BasicOperator.MULTIPLY:
+                    Answer = Operand1 * Operand2;
+                    break;
+                case BasicOperator.DIVIDE:
+                    Answer = Operand1 / Operand2;
+                    break;
+                default:
+                    Answer = 0;
+                    break;
+            }
+            return Answer.ToString();
+        }
+    }
+
+    public class PairMatching_QuestionPair
+    {
+        PairMathcing_Question Question1, Question2;
+        PairMatching_Difficulty Difficulty;
+        int CommonAnswer;
+
+        PairMatching_QuestionPair(int generation_range, PairMatching_Difficulty difficulty)
+        {
+
+        }
+
+        
+
+    }
+
+}
